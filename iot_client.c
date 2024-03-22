41d9ae68acebdbcdc1e47cc3241b7ff828596402
min9710 <154873930+min9710@users.noreply.github.com>
2024-03-22 19:31:12 +0900

    Add files via upload

--- iot_client.c
+++ iot_client.c
@@ -1,135 +1,93 @@
+
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <mysql/mysql.h>
-#include <pthread.h>
 #include <unistd.h>
-#include <sys/socket.h>
+#include <string.h>
 #include <arpa/inet.h>
-#include <time.h> // 시간 관련 헤더 파일 추가
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <pthread.h>
+#include <signal.h>
+#include <mysql/mysql.h>
 
 #define BUF_SIZE 100
 #define NAME_SIZE 20
 
-pthread_mutex_t mutex; // Mutex 선언
+void *send_msg(void *arg);
+void error_handling(char *msg);
 
-void * send_msg(void * arg);
-void * recv_msg(void * arg);
-void error_handling(char * msg);
-
-char name[NAME_SIZE]="[Default]";
+char name[NAME_SIZE] = "[Default]";
 char msg[BUF_SIZE];
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     int sock;
     struct sockaddr_in serv_addr;
-    pthread_t snd_thread, rcv_thread;
-    void * thread_return;
+    pthread_t snd_thread;
+    void *thread_return;
 
-    if(argc != 4) {
-        printf("Usage : %s <IP> <port> <name>\n",argv[0]);
+    if (argc != 4) {
+        printf("Usage : %s <IP> <port> <name>\n", argv[0]);
         exit(1);
     }
 
-    sprintf(name, "%s",argv[3]);
+    sprintf(name, "%s", argv[3]);
 
     sock = socket(PF_INET, SOCK_STREAM, 0);
-    if(sock == -1)
+    if (sock == -1)
         error_handling("socket() error");
 
     memset(&serv_addr, 0, sizeof(serv_addr));
-    serv_addr.sin_family=AF_INET;
+    serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
     serv_addr.sin_port = htons(atoi(argv[2]));
 
-    if(connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1)
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1)
         error_handling("connect() error");
 
-    sprintf(msg,"[%s:PASSWD]",name);
+    // 메시지 전송 스레드 생성
+    sprintf(msg, "[%s:PASSWD]", name);
     write(sock, msg, strlen(msg));
-    pthread_create(&rcv_thread, NULL, recv_msg, (void *)&sock);
     pthread_create(&snd_thread, NULL, send_msg, (void *)&sock);
 
     pthread_join(snd_thread, &thread_return);
+
     close(sock);
     return 0;
 }
 
-void * send_msg(void * arg)
-{
+void *send_msg(void *arg) {
     int *sock = (int *)arg;
-    char servo_msg[BUF_SIZE];
-    int str_len;
-    MYSQL *conn;
-    MYSQL_RES *res;
-    MYSQL_ROW row;
-    char *server = "localhost";
-    char *user = "cwb";
-    char *password = "1234";
-    char *database = "wild";
+    MYSQL *conn = mysql_init(NULL);
 
-    conn = mysql_init(NULL);
-    if (!mysql_real_connect(conn, server, user, password, database, 0, NULL, 0)) {
-        fprintf(stderr, "%s\n", mysql_error(conn));
+    // MySQL 연결 설정
+    if (conn == NULL) {
+        fprintf(stderr, "mysql_init() failed\n");
         exit(1);
     }
 
-    while(1) {
-        pthread_mutex_lock(&mutex); // Mutex 락
-
-        // 현재 시간을 가져와서 문자열로 변환
-        time_t now = time(NULL);
-	time_t one_minute_ago = now - 2;
-        struct tm *tm_info = localtime(&one_minute_ago);
-        char current_time[20];
-        strftime(current_time, sizeof(current_time), "%Y-%m-%d %H:%M:%S", tm_info);
-
-        // 데이터베이스에서 새로운 데이터 확인
-        char query[100];
-        sprintf(query, "SELECT * FROM wild_animal WHERE capture_datetime > '%s'", current_time);
-        if (mysql_query(conn, query)) {
-            fprintf(stderr, "%s\n", mysql_error(conn));
-            exit(1);
-        }
-        res = mysql_use_result(conn);
-        if ((row = mysql_fetch_row(res)) != NULL) {
-            // 데이터베이스에서 새로운 데이터가 있을 때 서보모터 제어 명령 전송
-            sprintf(servo_msg, "[%s]SERVO\n", "KSH_BT");
-	    printf(servo_msg);
-            write(*sock, servo_msg, strlen(servo_msg));
-        }
-        mysql_free_result(res);
-
-        pthread_mutex_unlock(&mutex); // Mutex 언락
-
-        // 일정 시간 간격으로 반복 확인
-        sleep(2);
+    if (mysql_real_connect(conn, "10.10.14.64", "lmg", "1234", "wild", 0, NULL, 0) == NULL) {
+        fprintf(stderr, "mysql_real_connect() failed\n");
+        mysql_close(conn);
+        exit(1);
     }
+
+    // 데이터베이스에 새로운 값을 추가하는 쿼리 실행
+    if (mysql_query(conn, "INSERT INTO wild_animal (species, area) VALUES ('곰', '지리산')") == 0) {
+        // INSERT 쿼리가 성공했을 때만 클라이언트가 서버에게 메시지 전송
+        sprintf(msg, "[LMG_BT]SERVO");
+        write(*sock, msg, strlen(msg));
+    } else {
+        fprintf(stderr, "mysql_query() failed\n");
+    }
+
+    // 연결 종료
+    mysql_close(conn);
+    return NULL;
 }
 
-void * recv_msg(void * arg)
-{
-    int * sock = (int *)arg;
-    char name_msg[NAME_SIZE + BUF_SIZE +1];
-    int str_len;
-    while(1) {
-        memset(name_msg,0x0,sizeof(name_msg));
-        str_len = read(*sock, name_msg, NAME_SIZE + BUF_SIZE );
-        if(str_len <= 0)
-        {
-            *sock = -1;
-            return NULL;
-        }
-        name_msg[str_len] = 0;
-        fputs(name_msg, stdout);
-    }
-}
-
-void error_handling(char * msg)
-{
+void error_handling(char *msg) {
     fputs(msg, stderr);
     fputc('\n', stderr);
     exit(1);
 }
-
